c6ced71ff21c96874175645ba150c69151607956
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-10-01 15:27:44 -0700

    Added put in tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -48,14 +48,24 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
-
-  
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },  
+    });
+    if (!updateTag) {
+      res.status(404).json({ message: 'No Tag with this id!' });
+      return;
+    } else {
+    res.status(200).json(updateTag);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-
-
 
 router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
